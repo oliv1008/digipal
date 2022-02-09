5dd0a63e0b1bfb02de54d0ab8de359cb1f584d61
oliv1008 <omillochau@gmail.com>
2022-02-09 18:15:54 +0100

    Ajout d'une tooltip relative à l'annotation au survol des points de chaque ligne du graph

--- digipal_project/customisations/static/digipal_project/scripts/visualizations.js
+++ digipal_project/customisations/static/digipal_project/scripts/visualizations.js
@@ -82,19 +82,21 @@
 
 let textsSelected = [];
 let annotationSelected = undefined;
+let data = new Map;
 
 function onTextSelected(button) {
     if (button.classList.contains("active")) {
         button.classList.remove("active");
         let index = textsSelected.indexOf(button.dataset.title.trim());
         textsSelected.splice(index, 1);
+        data.delete(button.dataset.title.trim());
     }
     else {
         button.classList.add("active");
         textsSelected.push(button.dataset.title.trim());
     }
 
-    extractData();
+    constructData();
 }
 
 function onAnnotationSelected(button) {
@@ -105,76 +107,55 @@ function onAnnotationSelected(button) {
 
     button.classList.add("active");
     annotationSelected = button.dataset.code;
-    extractData();
+    data = new Map;
+    constructData();
 }
 
-// function constructData() {
-//     if (textsSelected.length > 0 && annotationSelected) {
-//         switch (annotationSelected) {
-//             case "PROS-EV-PSY":
-//                 textsSelected.forEach( text => {
-//                     if (!data.has(text)) {
-//                         let dataAnnotation = [];
-//                         characteristics_as_json[3]["codes"]["#PROS-EV-PSY-BON"]["annotations"][text].forEach( value => {
-//                             dataAnnotation.push("#PROS-EV-PSY-BON", value["position_percentage"])
-//                         });
-//                     }
-//                 });
-//         }
-//     }
-// }
-
-function extractData() {
-    let data = new Map;
-
+// Construct the data dictionnary to be visualized from the characteristics dictionnary 
+function constructData() {
     if (textsSelected.length > 0 && annotationSelected) {
         switch (annotationSelected) {
             case "PROS-EV-PSY":
-                // We're iterating through every annotations_codes,
-                characteristics_as_json.forEach(characteristics => {
-                    for (const [keyCode, valueCode] of Object.entries(characteristics['codes'])) {
-                        // When we reach the annotations regarding the psychological status
-                        if (keyCode.includes("PROS-EV-PSY")) {
-                            for (const [keyAnnotation, valueAnnotation] of Object.entries(valueCode['annotations'])) {
-                                // In one the previously selected texts
-                                if (textsSelected.includes(keyAnnotation)) {
-                                    // We add every annotations and its position in that text to an array of shape (annotation_code, position)
-                                    let dataAnnotation = [];
-                                    valueAnnotation.forEach(valueAnnotationElement => {
-                                        dataAnnotation.push([keyCode, valueAnnotationElement['position_percentage']]) 
-                                    }); 
-                                    // We then add this array to the data dictionnary
-                                    // If an entry already exist, it's updated, if not, it's created. 
-                                    if (data.get(keyAnnotation)) {
-                                        data.set(keyAnnotation, data.get(keyAnnotation).concat(dataAnnotation));
-                                    }
-                                    else {
-                                        data.set(keyAnnotation, dataAnnotation);
-                                    }
-                                    
-                                }
+                // For each selected texts
+                textsSelected.forEach( text => {
+                    // If the data does not already contains the text informations 
+                    if (!data.has(text)) {
+                        let dataAnnotation = [];
+                        // We're iterating through all of the annotations code for that characteristic 
+                        for (const [keyCode, valueCode] of Object.entries(characteristics_as_json[3]["codes"])) {
+                            // If the text contains annotations regarding that characteristic
+                            if (valueCode["annotations"][text] != undefined) {
+                                // We're adding them and their position
+                                valueCode["annotations"][text].forEach( value => {
+                                    dataAnnotation.push(new Map([["annotation_code", keyCode], 
+                                                                ["position", value["position_percentage"]], 
+                                                                ["content", value["content"]], 
+                                                                ["url", value["url"]]]))
+                                })
                             }
                         }
+                        // We then sort the annotations in ascending order of their appearance for each text
+                        dataAnnotation.sort((a,b) => a.get("position") - b.get("position"));
+
+                        data.set(text, dataAnnotation);
                     }
                 });
-                // We then sort the annotations in ascending order of their appearance for each text
-                data.forEach((value, key, map) => value.sort((a,b) => a[1] - b[1]));
 
                 // In the end, the dictionnary looks like this :
                 // {
-                //     "text1" : [[annotation_code, position], ..., [annotation_code, position]],
+                //     "text1" : [{annotation_code, position, content, url}, ..., {annotation_code, position, content, url}],
                 //     ...,
-                //     "text5" : [[annotation_code, position], ..., [annotation_code, position]]
+                //     "text5" : [{annotation_code, position, content, url}, ..., {annotation_code, position, content, url}]
                 // }
 
                 break;
         }
     }
 
-    drawResult(data);
+    drawResult();
 }
 
-function drawResult(data) {
+function drawResult() {
     console.log(textsSelected);
     console.log(data);
     d3.select("#graph").selectChildren().remove();
@@ -185,6 +166,95 @@ function drawResult(data) {
                 break;
         }
     }
+}
+
+// Show the tooltip containing the sentence annoted
+function mouseEntered(event) {
+    // We retrieve the arguments passed with the ".bind()" method
+    const data = this.data
+    const xScale = this.xScale;
+    const yScale = this.yScale;
+    const tooltip = this.tooltip;
+
+    // We retrieve the value of the annotation which fired the event from the data dictionnary
+    // (We could have passed as an argument the value of the annotation directly from line/dots drawing loop but the tooltip would have been drawn over by the next line)
+    let content;
+    let index;
+    let keyIndex;
+    let position;
+    let annotation_code;
+    for (const [key, value] of data) {
+        index = value.findIndex( (element) => element === event.currentTarget.__data__);
+        if (index != -1) {
+            keyIndex = key;
+            content = value[index].get("content");
+            position = value[index].get("position");
+            annotation_code = value[index].get("annotation_code");
+            break;
+        }
+    }
+
+    tooltip.style("display", null);
+    tooltip.attr("transform", `translate(${xScale(position)},${yScale(annotation_code) -  50})`);
+
+    // The tooltip panel
+    const path = tooltip.selectAll("path")
+    .data([,])
+    .join("path")
+        .attr("fill", "white")
+        .attr("stroke", "black");
+
+    // The tooltip content
+    const text = tooltip.selectAll("text")
+    .data([,])
+    .join("text")
+    .call(text => text
+        .selectAll("tspan")
+        .data(content.split(/\n/))
+        .join("tspan")
+        .attr("x", 0)
+        .text(d => d));
+
+    // We're drawing the text and the panel (l-5,5 l-5,-5 is for the arrow) and centering them
+    const {x, y, width: w, height: h} = text.node().getBBox();
+    const margin = ({top : 10, right : 10, bottom : 10, left : 10});
+    text.attr("transform", `translate(${-w / 2}, ${27})`);
+    path.attr("d", `M${-w / 2 - margin.left}, ${margin.top}
+                    h${w + margin.left + margin.right}
+                    v${h + margin.bottom}
+                    h${-w / 2 - margin.right + 5}
+                    l-5,5
+                    l-5,-5
+                    h${-w / 2 - margin.left + 5}
+                    v${-h - margin.top}`);
+}
+
+function mouseLeft(event) {
+    const tooltip = this.tooltip;
+    const svg = this.svg;
+    
+    tooltip.style("display", "none");
+    svg.node().value = null;
+}
+
+// Open a new tab with the annotation highlighted in its text
+function click(event) {
+    // We retrieve the value of the annotation which fired the event from the data dictionnary
+    // (We could have passed as an argument the value of the annotation directly from line/dots drawing loop but the tooltip would have been drawn over by the next line)
+    let url;
+    let index;
+    for (const [key, value] of data) {
+        index = value.findIndex( (element) => element === event.currentTarget.__data__);
+        if (index != -1) {
+            url = value[index].get("url");
+            break;
+        }
+    }
+    // I could have used window.open() but it open a new tab or a new window depending on the user's browser settings, this always open a new tab
+    a = document.createElement("a");
+    a.setAttribute("href", url);
+    a.setAttribute("target", "_blank"); 
+    a.click()
 }
 
 function drawPsychologicalGraph(data) {
@@ -223,7 +293,7 @@ function drawPsychologicalGraph(data) {
         .attr("transform", `translate(0, ${height - margin.bottom})`)
         .call(xAxis)
         .call(g => g.append("text")
-            .attr("x", 350)
+            .attr("x", 460)
             .attr("y", margin.bottom)
             .attr("fill", "currentColor")
             .attr("text-anchor", "start")
@@ -244,8 +314,8 @@ function drawPsychologicalGraph(data) {
         }
         
         // Compute values.
-        const X = d3.map(value, d => d[1]);
-        const Y = d3.map(value, d => d[0]);
+        const X = d3.map(value, d => d.get("position"));
+        const Y = d3.map(value, d => d.get("annotation_code"));
         const I = d3.range(X.length);
 
         // Construct a line generator.
@@ -262,19 +332,21 @@ function drawPsychologicalGraph(data) {
             .attr("stroke-linecap", strokeLinecap)
             .attr("stroke-linejoin", strokeLinejoin)
             .attr("stroke-opacity", strokeOpacity)
-            .attr("d", line(I));
+            .attr("d", line(I))
+            .style("pointer-events", "none");
 
         // The dots corresponding to the annotations in the text
         svg.append("g")
-                .attr("stroke", "#000")
-                .attr("stroke-opacity", 0.2)
+            .attr("stroke", "#000")
+            .attr("stroke-opacity", 0.2)
             .selectAll("circle")
             .data(value)
             .join("circle")
-                .attr("cx", d => xScale(d[1])) // We set the position thanks the scale which give us the absolute value.
-                .attr("cy", d => yScale(d[0]))
-                .attr("fill", d => (d[0] === "#PROS-EV-PSY-BON") ? "blue" : "red")
-                .attr("r", 5);
+                .attr("cx", d => xScale(d.get("position"))) // We set the positions thanks to the scales which give us the absolute values.
+                .attr("cy", d => yScale(d.get("annotation_code")))
+                .attr("fill", d => (d.get("annotation_code") === "#PROS-EV-PSY-BON") ? "blue" : "red")
+                .attr("r", 5)
+                .style("cursor", "pointer")
 
         // The legend
         svg.append("line")
@@ -294,7 +366,18 @@ function drawPsychologicalGraph(data) {
            .attr("y", margin.top + 4 + iter * 50)
            .attr("textLength", 100)
            .text(key.slice(0, 10).concat("..."))
-    }) 
+    })
+
+    // The tooltip appearing on mouseover 
+    // We draw it last because we want it over every other element (lines and dots)
+    const tooltip = svg.append("g")
+        .style("pointer-events", "none");
+    
+    // The use of ".bind()" is one of the only way to pass parameters to an event handler, it replace the result of the "this" keywork with the value of the argument
+    svg.selectAll("circle")
+       .on("mouseenter", mouseEntered.bind({"data" : data, "xScale" : xScale, "yScale" : yScale, "tooltip" : tooltip}))
+       .on("mouseleave", mouseLeft.bind({"tooltip" : tooltip, "svg" : svg}))
+       .on("click", click.bind({"data" : data}));
         
     d3.select("#graph").node().append(svg.node());
 }
